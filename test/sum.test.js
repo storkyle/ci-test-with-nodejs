87260c0522c0d9f1c73393eaa13d879223cb766e
StorkyLe <quangnhat2429@gmail.com>
2020-02-12 15:12:25 +0700

    test ci trigger fails case with mail notifier

--- test/sum.test.js
+++ test/sum.test.js
@@ -2,7 +2,7 @@ import { calc } from "../utils";
 
 test("test function sum() in utils", () => {
   const result = calc.sum(1, 2, -3);
-  expect(result).toBe(0);
+  expect(result).toBe(6);
 });
 
 test("test function sum() with array string arguments", () => {
