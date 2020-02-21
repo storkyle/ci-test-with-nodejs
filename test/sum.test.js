44dbd183c125562aeef8e0a3def65bf6e4f5b40d
StorkyLe <quangnhat2429@gmail.com>
2020-02-21 17:58:07 +0700

    fix confict

--- test/sum.test.js
+++ test/sum.test.js
@@ -7,9 +7,5 @@ test("test function sum() in utils", () => {
 
 test("test function sum() with array string arguments", () => {
   const result = calc.sum("1", "2", "3");
-<<<<<<< HEAD
-  expect(result).toBe("12d");
-=======
   expect(result).toBe("12");
->>>>>>> ba5c92e889eeba8db2f87d71e0a7362897cf4a0e
 });
